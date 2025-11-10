ae14ecab23173711665116fcd5d7a8c35f8c4d20
Sosina Girmay Mehari <sosina.girmay@aastustudent.edu.et>
2025-11-11 01:05:20 +0300

    Time: 95 ms (14.82%) | Memory: 20.5 MB (46.01%) - LeetSync

--- 80-remove-duplicates-from-sorted-array-ii/remove-duplicates-from-sorted-array-ii.py
+++ 80-remove-duplicates-from-sorted-array-ii/remove-duplicates-from-sorted-array-ii.py
@@ -1,19 +1,19 @@
-# from typing import List
-
 class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:
-        i = 0
-        j = 2
-        length = len(nums)
+        i, j = 0, 2
         while j < len(nums):
-            if nums[i] != nums[j]:
+            if nums[i] == nums[j]:
+                nums[i] = '_'
+            i += 1
+            j += 1
+        
+        i, j = 0, 0
+        while j < len(nums):
+            if nums[j] != '_':
+                if nums[i] == '_':
+                    nums[i], nums[j] = nums[j], nums[i]
                 i += 1
-                j += 1
-            else:
-                nums.pop(j)
-        return len(nums)
+            j += 1
 
-prob = Solution()
-nums = [0,0,1,1,1,1,2,3,3]
-print(prob.removeDuplicates(nums))
-print(nums)
+        return i
+                
