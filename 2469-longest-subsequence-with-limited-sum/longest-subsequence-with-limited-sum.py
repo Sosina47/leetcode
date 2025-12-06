f9e91b981b16d3304e5b4caae50457277d5c272f
Sosina Girmay Mehari <sosina.girmay@aastustudent.edu.et>
2025-12-07 01:31:49 +0300

    Time: 39 ms (24.34%) | Memory: 18 MB (70.12%) - LeetSync

--- 2469-longest-subsequence-with-limited-sum/longest-subsequence-with-limited-sum.py
+++ 2469-longest-subsequence-with-limited-sum/longest-subsequence-with-limited-sum.py
@@ -1,20 +1,61 @@
 class Solution():
-    def answerQueries(self, nums: List[int], queries: List[int]):
-        nums.sort()
-        length = len(nums)
-        
-        # prefix sum
-        for index in range(1, length):
-            nums[index] = nums[index] + nums[index - 1]
-        
-        answer = []
-        for qIndex in range(len(queries)):
-            for nIndex in range(length):
-                if nums[nIndex] > queries[qIndex]:
-                    answer.append(nIndex)
-                    break
-                
-                if nIndex == length - 1 and nums[nIndex] <= queries[qIndex]:
-                    answer.append(nIndex + 1)
-        return answer
+    def merge(self, nums, low, mid, high):
+        left = low
+        right = mid + 1
+
+        result = []
+        while left <= mid and right <= high:
+            if nums[left] <= nums[right]:
+                result.append(nums[left])
+                left += 1
+            else:
+                result.append(nums[right])
+                right += 1
+
+        while left <= mid:
+            result.append(nums[left])
+            left += 1
+
+        while right <= high:
+            result.append(nums[right])
+            right += 1
+
+        for index in range(len(result)):
+            nums[low + index] = result[index]
+            
+            
+    def merge_sort(self, nums, low, high):
+        if low == high:
+            return 
+
+        mid = (low + high) // 2
+        self.merge_sort(nums, low, mid)  
+        self.merge_sort(nums, mid + 1, high)
+
+        self.merge(nums, low, mid, high)
     
+    def answerQueries(self, nums: List[int], queries: List[int]):
+        self.merge_sort(nums, 0, len(nums) - 1)
+        
+        length = len(nums)
+        for index in range(1, length):
+            nums[index] += nums[index - 1]
+            
+        def bin_search(nums, q):
+            left = 0
+            right = len(nums)
+            
+            while left < right:
+                mid = (left + right) // 2
+                if nums[mid] <= q:
+                    left = mid + 1
+                else:
+                    right = mid
+                    
+            return left
+        
+        result = []
+        for q in queries:
+            result.append(bin_search(nums, q))
+        
+        return result
