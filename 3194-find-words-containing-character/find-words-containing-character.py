8861810cad6ef1110ba2904c0fcef8886ec54cd7
Sosina Girmay Mehari <sosina.girmay@aastustudent.edu.et>
2025-12-18 22:05:29 +0300

    Time: 0 ms (100.00%) | Memory: 17.3 MB (100.00%) - LeetSync

---
+++ 3194-find-words-containing-character/find-words-containing-character.py
@@ -0,0 +1,10 @@
+from typing import List
+
+class Solution:
+    def findWordsContaining(self, words: List[str], x: str) -> List[int]:
+        output = []
+        for i in range(len(words)):
+            if x in words[i]:
+                output.append(i)
+
+        return output
