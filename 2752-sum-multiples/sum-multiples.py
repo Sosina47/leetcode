4335812cfbca7b4917afe15f854bfc9a4ace8265
Sosina Girmay Mehari <sosina.girmay@aastustudent.edu.et>
2025-12-30 10:22:15 +0300

    Time: 19 ms (91.22%) | Memory: 17.3 MB (86.79%) - LeetSync

---
+++ 2752-sum-multiples/sum-multiples.py
@@ -0,0 +1,8 @@
+class Solution:
+    def sumOfMultiples(self, n: int) -> int:
+        total = 0
+        for i in range(1, n + 1):
+            if not i % 3 or not i % 5 or not i % 7:
+                total += i
+
+        return total
