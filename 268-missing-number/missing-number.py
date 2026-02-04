c075d0ccc16e61d5575882ea5d5b9f23cff58785
Sosina Girmay Mehari <sosina.girmay@aastustudent.edu.et>
2026-02-04 15:56:52 +0300

    Time: 6 ms (38.36%) | Memory: 21.1 MB (5.47%) - LeetSync

--- 268-missing-number/missing-number.py
+++ 268-missing-number/missing-number.py
@@ -1,19 +1,30 @@
+# class Solution:
+#     def missingNumber(self, nums: List[int]) -> int:
+#         i = 0
+#         while i < len(nums):
+#             if nums[i] >= len(nums):
+#                 i += 1
+#                 continue
+#             correct_index = nums[i]
+#             if nums[i] != i:
+#                 nums[i], nums[correct_index] = nums[correct_index], nums[i]
+#             else:
+#                 i += 1
+
+#         for i in range(len(nums)):
+#             if nums[i] != i:
+#                 return i
+        
+#         # if there are 0 - n numbers, the missing is n
+#         return len(nums)
+
+
+
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
-        i = 0
-        while i < len(nums):
-            if nums[i] >= len(nums):
-                i += 1
-                continue
-            correct_index = nums[i]
-            if nums[i] != i:
-                nums[i], nums[correct_index] = nums[correct_index], nums[i]
-            else:
-                i += 1
+        n = len(nums)
+        nums = set(nums)
 
-        for i in range(len(nums)):
-            if nums[i] != i:
+        for i in range(n + 1):
+            if i not in nums:
                 return i
-        
-        # if there are 0 - n numbers, the missing is n
-        return len(nums)
