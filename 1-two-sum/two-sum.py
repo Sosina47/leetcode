5631b91f2e2bc9957cbbdb64d9a06d8b506e5f83
Sosina Girmay Mehari <sosina.girmay@aastustudent.edu.et>
2026-02-09 14:35:00 +0300

    Time: 0 ms (100.00%) | Memory: 20.5 MB (25.87%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -1,6 +1,52 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
+        idx = {nums[i]: i for i in range(len(nums))}
+
         for i in range(len(nums)):
-            for j in range(i + 1, len(nums)):
-                if nums[i] + nums[j] == target:
-                    return [i, j]
+            if target - nums[i] in idx and i != idx[target - nums[i]]:
+                return (i, idx[target - nums[i]])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        # prefix = [0] * len(nums)
+        # pos = {0: -1}
+
+        # for i in range(len(nums)):
+        #     if i == 0:
+        #         prefix[i] = nums[i]
+        #     else:
+        #         prefix[i] = nums[i] + prefix[i - 1]
+            
+        #     if prefix[i] - target in pos:
+        #         return [pos[prefix[i] - target] + 1, i]
+
+        #     pos[prefix[i]] = i
