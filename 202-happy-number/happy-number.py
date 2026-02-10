61a1faeed85a00eee09255733a16cd47c0e7ad69
Sosina Girmay Mehari <sosina.girmay@aastustudent.edu.et>
2026-02-10 14:34:28 +0300

    Time: 0 ms (100.00%) | Memory: 19.5 MB (16.31%) - LeetSync

--- 202-happy-number/happy-number.py
+++ 202-happy-number/happy-number.py
@@ -1,15 +1,54 @@
 class Solution:
     def isHappy(self, n: int) -> bool:
-        nums = set()
-        while n not in nums:
-            nums.add(n)
-            temp = 0
-            while n != 0:
-                temp += (n % 10) ** 2
-                n //= 10
-            n = temp
+        s = set()
+        while n != 1:
+            c = []
+            for i in str(n):
+                c.append(int(i) ** 2)
+            n = sum(c)
+            if n in s:
+                return False
 
-            if n == 1:
-                return True
+            s.add(n)
 
-        return False 
+        return True
+
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# class Solution:
+#     def isHappy(self, n: int) -> bool:
+#         nums = set()
+#         while n not in nums:
+#             nums.add(n)
+#             temp = 0
+#             while n != 0:
+#                 temp += (n % 10) ** 2
+#                 n //= 10
+#             n = temp
+
+#             if n == 1:
+#                 return True
+
+#         return False 
