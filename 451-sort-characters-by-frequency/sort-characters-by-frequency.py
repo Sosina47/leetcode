521846f566c5eea36e19b0b2b4f952e181cac43c
Sosina Girmay Mehari <sosina.girmay@aastustudent.edu.et>
2026-02-11 20:37:23 +0300

    Time: 11 ms (55.45%) | Memory: 21.3 MB (21.12%) - LeetSync

--- 451-sort-characters-by-frequency/sort-characters-by-frequency.py
+++ 451-sort-characters-by-frequency/sort-characters-by-frequency.py
@@ -1,25 +1,14 @@
 class Solution:
     def frequencySort(self, s: str) -> str:
-        s_freq = Counter(s)
-        s_items = list(s_freq.items())
+        frequency = Counter(s)
 
-        for i in range(1, len(s_items)):
-            j = i - 1
-            key = s_items[i]
-            while j >= 0:
-                if s_items[j][1] < key[1]:
-                    s_items[j + 1] = s_items[j]
-                    j -= 1
-                else: 
-                    break
-            s_items[j + 1] = key
+        freq = list(frequency.items())
+        freq.sort(key=lambda x: x[1], reverse=True)
 
-        output = ''
-        for i in range(len(s_items)):
-            val = s_items[i][0]
-            freq = s_items[i][1]
-            while freq > 0:
-                output += val
-                freq -= 1
+        output = []
+        for key in freq:
+            for i in range(key[1]):
+                output.append(key[0])
 
-        return output
+        return ''.join(output)
+        
