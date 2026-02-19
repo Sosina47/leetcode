5a8010da6779684646efae76f8f1f54dd46722f8
Sosina Girmay Mehari <sosina.girmay@aastustudent.edu.et>
2026-02-19 16:47:34 +0300

    Time: 2 ms (79.96%) | Memory: 19.4 MB (18.10%) - LeetSync

--- 1482-how-many-numbers-are-smaller-than-the-current-number/how-many-numbers-are-smaller-than-the-current-number.py
+++ 1482-how-many-numbers-are-smaller-than-the-current-number/how-many-numbers-are-smaller-than-the-current-number.py
@@ -1,17 +1,18 @@
 class Solution:
     def smallerNumbersThanCurrent(self, nums: List[int]) -> List[int]:
-        sortedNums = sorted(nums)
-        dictNums = {}
-        for i in range(len(sortedNums)):
-            num = sortedNums[i]
-            if num not in dictNums:
-                dictNums[num] = i
-                
+        frequency = Counter(nums)
+        unique_nums = sorted(list(set(nums)))
+
+        position = {val : i for i, val in enumerate(unique_nums)}
+        prefix_sum = [0]
+
+        for i in range(len(unique_nums)):
+            unique_nums[i] = frequency[unique_nums[i]]
+        
+        for i in range(len(unique_nums)):
+            prefix_sum.append(prefix_sum[-1] + unique_nums[i])
+
         for i in range(len(nums)):
-            nums[i] = dictNums[nums[i]]
+            nums[i] = prefix_sum[position[nums[i]]]
 
         return nums
-
-
-
-
