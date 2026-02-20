1690285c8dde3d4b3f45ef1cf6be9832359cd714
Sosina Girmay Mehari <sosina.girmay@aastustudent.edu.et>
2026-02-20 17:15:29 +0300

    Time: 2 ms (94.61%) | Memory: 23.7 MB (5.28%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.py
+++ 229-majority-element-ii/majority-element-ii.py
@@ -1,53 +1,10 @@
-# class Solution:
-#     def majorityElement(self, nums: List[int]) -> List[int]:
-#         nums_counter = Counter(nums)
-#         output = []
-#         for i in nums_counter:
-#             if nums_counter[i] > len(nums) // 3:
-#                 output.append(i)
-
-#         return output
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 class Solution:
     def majorityElement(self, nums: List[int]) -> List[int]:
-        lst = Counter(nums)
-        n = len(nums)
+        n = math.ceil(len(nums) // 3)
+        freq = Counter(nums)
         output = []
-        
-        for key in lst:
-            if lst[key] > n//3:
+        for key, val in freq.items():
+            if val > n:
                 output.append(key)
 
         return output
