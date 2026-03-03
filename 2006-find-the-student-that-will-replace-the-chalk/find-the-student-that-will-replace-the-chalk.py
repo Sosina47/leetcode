2ccbc62a830a6260d342c227390d01daa42ac999
Sosina Girmay Mehari <sosina.girmay@aastustudent.edu.et>
2026-03-04 01:17:31 +0300

    Time: 35 ms (17.05%) | Memory: 31.5 MB (17.05%) - LeetSync

---
+++ 2006-find-the-student-that-will-replace-the-chalk/find-the-student-that-will-replace-the-chalk.py
@@ -0,0 +1,9 @@
+class Solution:
+    def chalkReplacer(self, chalk: List[int], k: int) -> int:
+        for i in range(1, len(chalk)):
+            chalk[i] += chalk[i - 1]
+
+        k %= chalk[-1]
+        for i in range(len(chalk)):
+            if chalk[i] > k:
+                return i
