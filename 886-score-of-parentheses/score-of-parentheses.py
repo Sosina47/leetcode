724177451e4821fbe10f5ec5ec9cc7e66737e8b8
Sosina Girmay Mehari <sosina.girmay@aastustudent.edu.et>
2026-03-11 00:01:57 +0300

    Time: 0 ms (100.00%) | Memory: 19.3 MB (82.75%) - LeetSync

--- 886-score-of-parentheses/score-of-parentheses.py
+++ 886-score-of-parentheses/score-of-parentheses.py
@@ -1,18 +1,15 @@
 class Solution:
     def scoreOfParentheses(self, s: str) -> int:
-        stack = []
         score = 0
         count_open = 0
-
         for i in range(len(s)):
             if s[i] == '(':
-                stack.append(s[i])
                 count_open += 1
             else:
                 count_open -= 1
-                if s[i - 1] == '(':
-                    score += 2 ** count_open
-                stack.pop()
-            
-        return score
-                
+                if s[i - 1] == s[i]:
+                    continue
+                    
+                score += 1 if count_open == 0 else 2 ** count_open
+
+        return score 
