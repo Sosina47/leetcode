86f1a1ec8f59e80fdc02b7d0dc51f9887d37a04c
Sosina Girmay Mehari <sosina.girmay@aastustudent.edu.et>
2026-03-17 16:43:19 +0300

    Time: 148 ms (21.92%) | Memory: 37.8 MB (32.88%) - LeetSync

--- 2539-minimum-number-of-operations-to-make-arrays-similar/minimum-number-of-operations-to-make-arrays-similar.py
+++ 2539-minimum-number-of-operations-to-make-arrays-similar/minimum-number-of-operations-to-make-arrays-similar.py
@@ -1,5 +1,7 @@
 class Solution:
     def makeSimilar(self, nums: List[int], target: List[int]) -> int:
+        # whenever we have +2 operation we have -2 operation so the only thing we gotta do is find how many +2 or -2 operations we are required to do
+
         nums.sort()
         nums_even = [num for num in nums if num % 2 == 0]
         nums_odd = [num for num in nums if num % 2 == 1]
@@ -8,24 +10,34 @@ class Solution:
         target_even = [num for num in target if num % 2 == 0]
         target_odd = [num for num in target if num % 2 == 1]      
 
-        diff = []
+        # diff = []
+        operations = 0
         for i in range(len(nums_even)):
-            diff.append(abs(nums_even[i] - target_even[i]) // 2)
+            # diff.append(abs(nums_even[i] - target_even[i]) // 2)
+            diff_ = target_even[i] - nums_even[i]
+            if diff_ > 0:
+                operations += diff_ // 2
         
         for i in range(len(nums_odd)):
-            diff.append(abs(nums_odd[i] - target_odd[i]) // 2)
+            # diff.append(abs(nums_odd[i] - target_odd[i]) // 2)
+            diff_ = target_odd[i] - nums_odd[i]
+            if diff_ > 0:
+                operations += diff_ // 2
+
+        return operations
         
-        diff.sort(reverse = True)
-        operations = diff[0] if diff else  0
-        cur_op = diff[0] if diff else  0
+        # diff.sort(reverse = True)
+        # operations = diff[0] if diff else  0
+        # cur_op = diff[0] if diff else  0
 
-        return sum(diff) // 2 
+        # return sum(diff) // 2 
+        # print(diff)
 
-        for d in diff[1:]:
-            if cur_op >= d:
-                cur_op -= d
-            else:
-                operations += d - cur_op
-                cur_op = d - cur_op
+        # for d in diff[1:]:
+        #     if cur_op >= d:
+        #         cur_op -= d
+        #     else:
+        #         operations += d - cur_op
+        #         cur_op = d - cur_op
                 
-        return operations 
+        # return operations 
