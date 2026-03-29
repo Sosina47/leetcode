5eedd63f2a17c541550b5e21e4d619c4ee8dc1bf
Sosina Girmay Mehari <sosina.girmay@aastustudent.edu.et>
2026-03-29 14:46:13 +0300

    Time: 3075 ms (40.77%) | Memory: 19.5 MB (43.86%) - LeetSync

---
+++ 473-matchsticks-to-square/matchsticks-to-square.py
@@ -0,0 +1,39 @@
+class Solution:
+    def makesquare(self, matchsticks: List[int]) -> bool:
+        n = len(matchsticks)
+        sticks = [0, 0, 0, 0]
+
+        summ = sum(matchsticks)
+        if summ % 4 != 0:
+            return False
+        
+        side = summ // 4
+        
+        def solve(index, sticks):            
+            if index == n:
+                if max(sticks) == min(sticks) and sticks[0] > 0:
+                    return True
+
+                return False
+
+            for i in range(4):
+                if i > 0 and sticks[i] == sticks[i - 1]:
+                    continue
+
+                if sticks[i] + matchsticks[index] <= side:
+                    sticks[i] += matchsticks[index]
+                    
+                    if solve(index + 1, sticks):
+                        return True
+
+                    sticks[i] -= matchsticks[index]
+
+                    if sticks[i] == 0:
+                        break
+
+            return False
+
+        return solve(0, sticks)
+
+
+                    
