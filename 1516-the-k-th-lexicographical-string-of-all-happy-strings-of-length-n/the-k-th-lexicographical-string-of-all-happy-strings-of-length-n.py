d0783a07382a01b4b67d4eb2c902666261af6d07
Sosina Girmay Mehari <sosina.girmay@aastustudent.edu.et>
2026-03-29 19:43:23 +0300

    Time: 2 ms (70.69%) | Memory: 20.3 MB (22.33%) - LeetSync

--- 1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.py
+++ 1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.py
@@ -7,16 +7,23 @@ class Solution:
         def solve(strr):
             if len(strr) == n:
                 output.append("".join(strr))
-                return 
+            
+                if len(output) == k:
+                    return True
+
+                return False
 
             for i in range(3):
                 if not strr or strr[-1] != happy[i]:
                     strr.append(happy[i])
 
-                    solve(strr)
+                    if solve(strr):
+                        return True
 
                     strr.pop()
 
+            return False
+
         solve([])
-        
+
         return output[k - 1] if k <= len(output) else ""
