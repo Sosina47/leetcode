cf2d2c0f02b161ba993fb32e6da72494c0839f1c
Sosina Girmay Mehari <sosina.girmay@aastustudent.edu.et>
2026-03-29 12:38:10 +0300

    Time: 2 ms (79.10%) | Memory: 19.3 MB (56.87%) - LeetSync

--- 1418-fair-distribution-of-cookies/fair-distribution-of-cookies.py
+++ 1418-fair-distribution-of-cookies/fair-distribution-of-cookies.py
@@ -1,5 +1,7 @@
 class Solution:
     def distributeCookies(self, cookies: List[int], k: int) -> int:
+        cookies.sort(reverse = True) # should first assign the larger ones (minimizes the runtime)
+        
         length = len(cookies)
         min_unfair = float("inf")
         
@@ -11,11 +13,20 @@ class Solution:
                 return 
 
             for i in range(k):
+                # u already tried giving the current cookie with a child the same # cookies
+                if i > 0 and nums[i] == nums[i - 1]:
+                    continue
+                    
                 nums[i] += cookies[start]
 
-                solve(start + 1, nums)
+                if nums[i] < min_unfair:
+                    solve(start + 1, nums)
                 
                 nums[i] -= cookies[start]
+
+                # if the current kid got 0 cookies, the rest of the kids after him also have 0 cookies. and if we try   assigning a cookie to him and if it didn't work, then it won't work for the rest of the kids who got 0 cookies like him
+                if nums[i] == 0:
+                    break
 
         solve(0, [0] * k)
         return min_unfair 
