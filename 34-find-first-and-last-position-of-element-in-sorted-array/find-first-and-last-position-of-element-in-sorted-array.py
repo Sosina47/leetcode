38c195579881412fee1a6e8ec49b20d66aa6f682
Sosina Girmay Mehari <sosina.girmay@aastustudent.edu.et>
2026-03-31 18:22:29 +0300

    Time: 0 ms (100.00%) | Memory: 20.5 MB (74.86%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.py
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.py
@@ -1,26 +1,30 @@
 class Solution:
     def searchRange(self, nums: List[int], target: int) -> List[int]:
-        if not nums:
-            return [-1, -1]
-        res = []
-        left, right = 0, len(nums) - 1
-        while left <= right:
-            mid = (left + right) // 2
-            if nums[mid] >= target:
-                right = mid - 1
-            else:
-                left = mid + 1
-        if left == len(nums) or nums[left] != target :
-            return [-1, -1]
-        res.append(left)
+        n = len(nums)
 
-        left, right = 0, len(nums) -1
-        while left <= right:
-            mid = (left + right) // 2
-            if nums[mid] <= target:
-                left = mid + 1
-            else:
-                right = mid - 1
-        res.append(right)
+        def findIndex(num):
+            left = 0
+            right = n - 1
 
-        return res
+            while right >= left:
+                mid = (right + left) // 2
+
+                if nums[mid] >= num:
+                    right = mid - 1
+                
+                else:
+                    left = mid + 1
+
+            return left 
+
+        first = findIndex(target) 
+        last = findIndex(target + 1) - 1
+
+        if first < 0 or first >= n or nums[first] != target:
+            first = -1
+            
+        if last < 0 or last >= n or nums[last] != target:
+            last = -1
+
+        return [first, last]
+        
