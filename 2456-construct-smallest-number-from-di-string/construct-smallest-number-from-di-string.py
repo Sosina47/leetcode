7d1e590efbfa8cc02d15ae4a6f9b426764703743
Sosina Girmay Mehari <sosina.girmay@aastustudent.edu.et>
2026-04-16 13:40:12 +0300

    Time: 433 ms (7.43%) | Memory: 30.3 MB (5.10%) - LeetSync

---
+++ 2456-construct-smallest-number-from-di-string/construct-smallest-number-from-di-string.py
@@ -0,0 +1,46 @@
+class Solution:
+    def smallestNumber(self, pattern: str) -> str:
+        seen = set()
+        output = []
+        n = len(pattern)
+
+        def solve(path):
+            if len(path) == n + 1:
+                output.append(''.join(map(str, path)))
+                return 
+
+            for i in range(1, 10): 
+                if not path:
+                    path.append(i)
+                    seen.add(i)
+
+                    solve(path)
+                    seen.remove(path.pop())
+
+                else:
+                    if i in seen:
+                        continue
+
+                    index = len(path) - 1
+
+                    if pattern[index] == 'I':
+                        if path[-1] < i:
+                            path.append(i)
+                            seen.add(i)
+
+                            solve(path)
+
+                            seen.remove(path.pop())
+
+                    else:
+                        if path[-1] > i:
+                            path.append(i)
+                            seen.add(i) 
+
+                            solve(path)
+
+                            seen.remove(path.pop())
+                        
+                
+        solve([])
+        return sorted(output)[0]
