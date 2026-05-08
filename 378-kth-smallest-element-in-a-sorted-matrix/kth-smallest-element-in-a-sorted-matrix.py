d7a6ec8e7b6927d9f91d3a0e23e0f347f206bb66
Sosina Girmay Mehari <sosina.girmay@aastustudent.edu.et>
2026-05-08 13:00:25 +0300

    Time: 23 ms (41.46%) | Memory: 23.7 MB (8.42%) - LeetSync

--- 378-kth-smallest-element-in-a-sorted-matrix/kth-smallest-element-in-a-sorted-matrix.py
+++ 378-kth-smallest-element-in-a-sorted-matrix/kth-smallest-element-in-a-sorted-matrix.py
@@ -1,19 +1,13 @@
 class Solution:
     def kthSmallest(self, matrix: List[List[int]], k: int) -> int:
-        nums = []
+        heap = []
+        n = len(matrix)
 
-        for r in range(len(matrix)):
-            for c in range(len(matrix[0])):
-                nums.append(matrix[r][c])
+        for r in range(n):
+            for c in range(n):
+                heappush(heap, -matrix[r][c])
 
-        nums.sort()
-        count = 0
+                if len(heap) > k: 
+                    heappop(heap)
 
-        # for i in range(len(nums)):
-        #     if i == 0 or nums[i] != nums[i - 1]:
-        #     count += 1
-
-        #     if count == k:
-        #         return nums[i]
-
-        return nums[k - 1]
+        return -heap[0]
