8a216b33f1ce252a1fb0736b1f500d3f838a7d87
Sosina Girmay Mehari <sosina.girmay@aastustudent.edu.et>
2026-05-10 22:35:30 +0300

    Time: 0 ms (100.00%) | Memory: 19.2 MB (51.92%) - LeetSync

---
+++ 70-climbing-stairs/climbing-stairs.py
@@ -0,0 +1,13 @@
+class Solution:
+    def __init__(self):
+        self.memo = {}
+    def climbStairs(self, n: int) -> int:
+        if n == 1: 
+            return 1
+        if n == 2: 
+            return 2
+        if n in self.memo:
+            return self.memo[n]
+        
+        self.memo[n] = self.climbStairs(n-2) + self.climbStairs(n-1)
+        return self.memo[n]
