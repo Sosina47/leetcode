1d30dca8dbdce91e6b6169108a27a910f213014e
Sosina Girmay Mehari <sosina.girmay@aastustudent.edu.et>
2026-06-27 21:56:41 +0300

    Time: 15 ms (57.37%) | Memory: 19.2 MB (86.52%) - LeetSync

---
+++ 657-robot-return-to-origin/robot-return-to-origin.py
@@ -0,0 +1,18 @@
+class Solution:
+    def judgeCircle(self, moves: str) -> bool:
+        x = y = 0
+
+        for c in moves: 
+            if c == 'U': 
+                y += 1
+
+            elif c == 'D': 
+                y -= 1
+
+            elif c == 'R': 
+                x += 1
+
+            else: 
+                x -= 1
+
+        return x == y == 0
