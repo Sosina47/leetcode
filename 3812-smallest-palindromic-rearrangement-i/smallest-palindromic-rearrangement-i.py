d9bada96f6556ec980a4177ae956650850731962
Sosina Girmay Mehari <sosina.girmay@aastustudent.edu.et>
2026-07-28 13:22:54 +0300

    Time: 242 ms (62.21%) | Memory: 21.9 MB (10.60%) - LeetSync

--- 3812-smallest-palindromic-rearrangement-i/smallest-palindromic-rearrangement-i.py
+++ 3812-smallest-palindromic-rearrangement-i/smallest-palindromic-rearrangement-i.py
@@ -1,10 +1,15 @@
 class Solution:
     def smallestPalindrome(self, s: str) -> str:
-        length = len(s)
-        
-        sorted_s = sorted(s[: length // 2])
-        letter = ''
-        if length % 2:
-            letter = s[length // 2]
+        hash = Counter(s)
+        h = sorted(hash)
+        mid = ''
 
-        return ''.join(sorted_s) + letter + ''.join(sorted_s[::-1])
+        s = []
+        for key in h: 
+            if not mid and hash[key] % 2 == 1: 
+                mid = key
+            
+            for _ in range(hash[key] // 2):
+                s.append(key)
+        
+        return ''.join(s) + mid + ''.join(s[::-1])
